7d5e4be66ee94970635e083956039fe3f1c0084a
tangentepisur4 <60540427+tangentepisur4@users.noreply.github.com>
2020-10-23 06:04:05 +0100

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://8fb5ccfa.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
